b643f799334abe6a9a1a5b462f0ba74482d80838
delafuentej <jfuentelucas@gmail.com>
2024-05-26 12:47:46 +0200

    using List Views with FlatList

--- app/forum.tsx
+++ app/forum.tsx
@@ -1,11 +1,42 @@
-import { Text } from "react-native";
+import { Text, FlatList, StyleSheet, View } from "react-native";
+
+
+const styles= StyleSheet.create({
+  container:{
+    flex: 1,
+    paddingTop:20,
+    margin: 'auto',
+  },
+  item:{
+    padding:10,
+    fontSize:20,
+    height:44,
+  },
+});
 
 
 export default function Forum() {
     return (
-      <>
-        <Text>Forum</Text>
+      <View 
+        style={styles.container}
+      >
+        <Text>FlatList Component</Text>
+        <FlatList
+        data={[
+          {key: 'James'},
+          {key: 'John'},
+          {key: 'Mary'},
+          {key: 'Martha'},
+          {key: 'Alfred'},
+          {key: 'Thomas'},
+          {key: 'Joana'},
+          {key: 'Jana'},
+          {key: 'Anna'},
+        ]}
+        renderItem={({item})=> <Text style={styles.item}>{item.key}</Text>}
+        />
+
         
-      </>
+      </View>
     );
   }
