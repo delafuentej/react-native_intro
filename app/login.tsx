5c689751e55d6a0ba48e34262ef5dd3bedc3f37b
delafuentej <jfuentelucas@gmail.com>
2024-05-26 11:51:37 +0200

    some core components in login added

--- app/login.tsx
+++ app/login.tsx
@@ -1,11 +1,48 @@
-import { Text } from "react-native";
+import { Text, Image, TextInput, View, ScrollView, StyleSheet } from "react-native";
 
+const styles= StyleSheet.create({
+  contentContainer: {
+   margin: 'auto',
+  }
+});
 
 export default function Login() {
     return (
-      <>
-        <Text>Login</Text>
+      <ScrollView style={styles.contentContainer}>
+        <Text style={{textAlign: 'center'}}>Login</Text>
+        <View>
+          <Text style={{textAlign: 'center'}}>Welcome!!</Text>
+          <Image
+            source={{
+              uri:'https://reactnative.dev/docs/assets/p_cat2.png',
+            }} 
+            style={{
+              width: 200,
+              height:200
+            }}
+            />
+        </View>
+        <TextInput
+          style={{
+            height:40,
+            width:200,
+            borderColor:'blue',
+            borderWidth:3,
+          }}
+          defaultValue="Type your username"
         
-      </>
+        />
+         <TextInput
+          style={{
+            height:40,
+            width:200,
+            borderColor:'blue',
+            borderWidth:3,
+          }}
+          defaultValue="Type your password"
+        
+        />
+        
+      </ScrollView>
     );
   }
